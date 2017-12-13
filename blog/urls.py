0a45a12233943f577ef755532682c57cd012e6a3
singll <lijian_note@163.com>
2017-12-13 20:26:14 +0800

    simple search

--- blog/urls.py
+++ blog/urls.py
@@ -21,4 +21,7 @@ urlpatterns = [
     url(r'^tag/(?P<pk>[0-9]+)/$',
         views.TagView.as_view(), name='tag'
         ),
+    url(r'^search/$',
+        views.search, name='search'
+        ),
 ]
